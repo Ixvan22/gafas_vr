f8127d3decf5145e138ea0141ffdc574995059e6
Ixvan22 <ivangarciacarrasco22@gmail.com>
2023-11-07 16:51:25 +0100

    Script cambio tema

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -4,15 +4,15 @@ const ICON_LIGHT = document.getElementById('navbar-theme-icon-sun');
 const ICON_DARK = document.getElementById('navbar-theme-icon-moon');
 
 
-function changeTheme(type) {
+function changeTheme(type, page) {
     if (type == "dark") {
         ICON_DARK.classList.toggle('navbar-theme-icon-hidden');
         ICON_LIGHT.classList.toggle('navbar-theme-icon-hidden');
-        STYLESHEET.setAttribute('href', 'css/index-main-dark.css');
+        STYLESHEET.setAttribute('href', `css/${page}-main-dark.css`);
     }
     else if (type == "light") {
         ICON_LIGHT.classList.toggle('navbar-theme-icon-hidden');
         ICON_DARK.classList.toggle('navbar-theme-icon-hidden');
-        STYLESHEET.setAttribute('href', 'css/index-main-light.css');
+        STYLESHEET.setAttribute('href', `css/${page}-main-light.css`);
     }
 }
