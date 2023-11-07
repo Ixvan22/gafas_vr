08d6a822536519c555d6a4f269f03145fa5d5473
Ixvan22 <ivangarciacarrasco22@gmail.com>
2023-11-07 17:27:19 +0100

    LocalStorage changetheme

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -1,18 +1,38 @@
-const STYLESHEET = document.getElementById('stylesheet-theme');
-
-const ICON_LIGHT = document.getElementById('navbar-theme-icon-sun');
-const ICON_DARK = document.getElementById('navbar-theme-icon-moon');
-
-
 function changeTheme(type, page) {
+
+    let STYLESHEET = document.getElementById('stylesheet-theme');
+
+    let ICON_LIGHT = document.getElementById('navbar-theme-icon-sun');
+    let ICON_DARK = document.getElementById('navbar-theme-icon-moon');
+
     if (type == "dark") {
-        ICON_DARK.classList.toggle('navbar-theme-icon-hidden');
-        ICON_LIGHT.classList.toggle('navbar-theme-icon-hidden');
+        ICON_DARK.classList.add('navbar-theme-icon-hidden');
+        ICON_LIGHT.classList.remove('navbar-theme-icon-hidden');
         STYLESHEET.setAttribute('href', `css/${page}-main-dark.css`);
+        localStorage.removeItem("stylesheet");
+        localStorage.setItem('stylesheet', 'dark');
     }
     else if (type == "light") {
-        ICON_LIGHT.classList.toggle('navbar-theme-icon-hidden');
-        ICON_DARK.classList.toggle('navbar-theme-icon-hidden');
+        console.log("Entra");
+        ICON_LIGHT.classList.add('navbar-theme-icon-hidden');
+        ICON_DARK.classList.remove('navbar-theme-icon-hidden');
+        console.log(`css/${page}-main-light.css`);
         STYLESHEET.setAttribute('href', `css/${page}-main-light.css`);
+        localStorage.removeItem("stylesheet");
+        localStorage.setItem('stylesheet', 'light');
     }
+}
+
+console.log(localStorage.getItem("stylesheet"));
+
+function translatePathName (pathname) {
+    if (pathname === "experiencias") pathname = "experiences";
+    else if (pathname === "soporte") pathname = "support";
+    return pathname;
+}
+
+if (localStorage.getItem("stylesheet") != null) {
+    let location = window.location.pathname;
+    let pathname = translatePathName(location.substring(1, location.length - 5));
+    changeTheme(localStorage.getItem('stylesheet'), pathname);
 }
