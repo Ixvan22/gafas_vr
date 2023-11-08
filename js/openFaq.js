7fec2e5868f4f0339cadbfa6c57ec8841a9e51c5
Ivan Garcia <ivangarciacarrasco22@gmail.com>
2023-11-08 11:48:04 +0100

    responsive faq

--- js/openFaq.js
+++ js/openFaq.js
@@ -1,13 +1,13 @@
+const CONTAINER_QUESTIONS = document.querySelectorAll('.questions-title-container');
 const QUESTIONS = document.querySelectorAll('.arrow-icon');
 const ANSWERS = document.querySelectorAll('.answer');
-console.log(ANSWERS);
 
-QUESTIONS.forEach((question, key) => {
+CONTAINER_QUESTIONS.forEach((question, key) => {
     let actual_key = key;
     
     question.addEventListener('click', () => {
         ANSWERS[actual_key].classList.toggle('answer-show');
-        question.classList.toggle('fa-arrow-down');
-        question.classList.toggle('fa-arrow-up');
+        QUESTIONS[key].classList.toggle('fa-arrow-down');
+        QUESTIONS[key].classList.toggle('fa-arrow-up');
     })
 })
