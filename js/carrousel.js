6fb4345352c3bc58069baafef939f9cb77fa065c
Ivan Garcia <ivangarciacarrasco22@gmail.com>
2023-11-09 13:11:28 +0100

    fix mas de un producto

--- js/carrousel.js
+++ js/carrousel.js
@@ -11,9 +11,21 @@ function cambio( sen){
  }
  
 function reset(){
+    let location = window.location.pathname;
+    let pathname = location.substring(location.lastIndexOf("/") + 1, location.length - 5);
+
     let carrusel = document.getElementById('center-image');
-    carrusel.lista = new Array('img/products/gafas1.png', 'img/products/gafas2.png', 'img/products/gafas3.png');
+    switch (pathname) {
+        case "producto-meta-quest-2":
+            console.log("aaa");
+            carrusel.lista = ['img/products/gafas1.png', 'img/products/gafas2.png', 'img/products/gafas3.png'];
+            break;
+        case "producto2":
+            carrusel.lista = ['img/products/gafas.png', 'img/products/gafas.png', 'img/products/gafas.png'];
+            break;
+    }
     carrusel.contador = 0;
     carrusel.tempor = null;
     carrusel.src = carrusel.lista[0]
 }
+
